0e66dcf37f40a25e051968282ff5bba723643b6a
Abdullah Najid <anajid@emich.edu>
2021-10-14 15:34:45 -0400

    Created client handler class and added run method.

--- src/ana_TCPServer.java
+++ src/ana_TCPServer.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class ana_TCPServer {
     private static ServerSocket serverSocket;
+    FileWriter myWriter;
+    Scanner myReader;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Opening port...\n");
 
         // Try to create a socket with a port number.
@@ -36,12 +39,59 @@ public class ana_TCPServer {
         }
     }
 
-    private static void connectionGetter() {
+    private static void connectionGetter() throws IOException {
         Socket link = null;
 
-        try {
-            link = serverSocket.accept();
-        } catch (IOException e) { e.printStackTrace(); }
+        link = serverSocket.accept();
+
+        long startTime = System.currentTimeMillis();
+
+        // Set up input and output streams for socket
+        BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+        PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+
+        // print local host name
+        String host = InetAddress.getLocalHost().getHostName();
+        System.out.println("Client has established a connection to " + host);
+
+        // First message from client is client's username.
+        String clientUsername = in.readLine();
+
+        ClientHandler clientHandler = new ClientHandler(link, in, out, startTime, clientUsername);
+        clientHandler.start();
+    }
+
+    public static class ClientHandler extends Thread {
+        Socket link;
+        BufferedReader in;
+        PrintWriter out;
+        long startTime;
+        String userName;
+
+        public ClientHandler(Socket link, BufferedReader in, PrintWriter out, long startTime, String userName) {
+            this.link = link;
+            this.in = in;
+            this.out = out;
+            this.startTime = startTime;
+            this.userName = userName;
+        }
+
+        public void run() {
+            String message = "";
+            try {
+                message = in.readLine();
+            } catch (IOException e) { e.printStackTrace(); }
+
+            while (!message.equals("DONE")) {
+                // Append client's username to their message and print it.
+                String formatMessage = userName + ": " + message;
+                System.out.println(formatMessage);
+
+                try {
+                    message = in.readLine();
+                } catch (IOException e) { e.printStackTrace(); }
+            }
+        }
     }
 
 }
